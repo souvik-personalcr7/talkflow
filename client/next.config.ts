b07c5866aea9f2c168f1734340ac98a0840489c7
Souvik <souvikdas8637@gmail.com>
2026-08-28 00:26:07 +0530

    feat: configure Next.js with dev origin allowance and bypass typescript build errors

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: any = {
   /* config options here */
   allowedDevOrigins: ['192.168.29.143'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
